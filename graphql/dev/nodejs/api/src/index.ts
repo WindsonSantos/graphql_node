4e422a88d33810ca017eddb43db2f5a1618c845e
Windson Santos <windsonsantos@wizsolucoes.com.br>
2018-08-05 19:52:41 -0300

    Adicionado graphql ao projeto e projeto rodando 100%% Criado gitignore para os a pasta node_modules

--- graphql/dev/nodejs/api/src/index.ts
+++ graphql/dev/nodejs/api/src/index.ts
@@ -2,7 +2,11 @@ import * as http from 'http';
 
 import app from './app';
 
-const server = http.createServer(app);
+import {normalizePort, onError, onListening} from './utils/utils';
 
-server.listen(3000);
-server.on('listening',()=>'Listenning on port 3000...');
+const server = http.createServer(app);
+const port = normalizePort(process.env.port || 3000);
+
+server.listen(port);
+server.on('error', onError(server));
+server.on('listening', onListening(server));
